c1aa5b7151eb6cf849b8b1e025d4da6d5e378e16
Lul-Abdifan <tasanbannu@gmail.com>
2023-06-25 04:23:38 -0400

    Update the person class to inherit Nameable class

--- person.rb
+++ person.rb
@@ -1,26 +1,39 @@
-class Person
-    attr_accessor :name, :age
-    attr_writer :id
-    
+require './interface'
+require './capitalize_decor'
+require './trim_decor'
 
-    def initialize(name = "Unknown", age, parent_permission = true)
-        @id = Random.rand(1..1000)
-        @name = name
-        @age = age
-        @parent_permission = parent_permission
-      end
-    
-      private
-    
-      def of_age?
-        @age > 18
-      end
-    
-      public
-    
-      def can_use_services?
-        of_age? || @parent_permission
-      end
-    end
+class Person < Nameable
+  attr_accessor :name, :age
+  attr_writer :id
 
-    
+  def initialize(age, name = 'Unknown', parent_permission: true)
+    super()
+    @id = Random.rand(1..1000)
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+  end
+
+  def correct_name
+    @name
+  end
+
+  private
+
+  def of_age?
+    @age > 18
+  end
+
+  public
+
+  def can_use_services?
+    of_age? || @parent_permission
+  end
+end
+
+person = Person.new(22, 'maximilianus')
+puts person.correct_name
+capitalized_person = CapitalizeDecorator.new(person)
+puts capitalized_person.correct_name
+capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
+puts capitalized_trimmed_person.correct_name
