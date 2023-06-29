ddb847e0d9e5d6e04205091c6065a6af48535416
Lul-Abdifan <tasanbannu@gmail.com>
2023-06-29 13:28:36 -0400

    Create a person function

--- app.rb
+++ app.rb
@@ -1,7 +1,9 @@
+require_relative "student"
+require_relative "person"
 class App
     def initialize
-       @book = []
-       @person = []
+       @books = []
+       @people = []
        @rentals = []
     end
 
@@ -14,9 +16,45 @@ class App
     end
 
     def create_person
-        puts "Person created"
+        puts "Do you want a student(1) or a teacher(2)? [input the number]:"
+        choice = gets.chomp.to_i
+        case choice
+        when 1
+        create_student
+        when 2
+        create_teacher
+        end
+
+
     end
+    def create_student
+        print "Age:"
+        age = Integer(gets.chomp)
+        print "Name:"
+        name = String(gets.chomp)
+        print "Has parent permission? [Y/N]:"
+        permission = gets.chomp.downcase
+      
+        case permission
+        when 'y'
+            student = Student.new(nil,age,name,parent_permission: true)
+        when 'n'
+            student = Student.new(nil,age,name,parent_permission: false)
+        else
+          puts "Incorrect input"
+          return
+        end
+
+
+        @people << student if student
+        puts "Person created successfully"
+      end
+      
+
     
+      
+      
+      
     def create_book
         puts "Book created"
     end
