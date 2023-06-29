173fbadd89ed1eb73cb2ec945ac2a2d2537454fb
Lul-Abdifan <tasanbannu@gmail.com>
2023-06-29 13:14:15 -0400

    Delete unused codes

--- person.rb
+++ person.rb
@@ -7,7 +7,7 @@ class Person < Nameable
   attr_writer :id
   attr_reader :rentals
 
-  def initialize(age, name = 'Unknown', parent_permission: true)
+  def initialize(age, name, parent_permission: true)
     super()
     @id = Random.rand(1..1000)
     @name = name
@@ -37,9 +37,4 @@ class Person < Nameable
   end
 end
 
-person = Person.new(22, 'maximilianus')
-puts person.correct_name
-capitalized_person = CapitalizeDecorator.new(person)
-puts capitalized_person.correct_name
-capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
-puts capitalized_trimmed_person.correct_name
+
