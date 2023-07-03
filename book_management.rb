e8d794f568e7a53e1fd72b09b5e2c29a0e83d8c7
Lul-Abdifan <tasanbannu@gmail.com>
2023-07-03 07:36:23 -0400

    Fix eslint errors

--- book_management.rb
+++ book_management.rb
@@ -1,20 +1,19 @@
 def create_book
-    puts 'Title:'
-    title = gets.chomp.to_s
-    puts 'Author:'
-    author = gets.chomp.to_s
-    book = Book.new(title, author)
-    @books.push(book)
-    puts "\nBook created successfully"
-  end
+  puts 'Title:'
+  title = gets.chomp.to_s
+  puts 'Author:'
+  author = gets.chomp.to_s
+  book = Book.new(title, author)
+  @books.push(book)
+  puts "\nBook created successfully"
+end
 
-  def list_all_books
-    if @books.empty?
-      puts "No books"
-    else
-      @books.each do |book|
-        puts "Title: '#{book.title}', Author: '#{book.author}'"
-      end
+def list_all_books
+  if @books.empty?
+    puts 'No books'
+  else
+    @books.each do |book|
+      puts "Title: '#{book.title}', Author: '#{book.author}'"
     end
   end
-  
+end
